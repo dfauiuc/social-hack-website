5e78d234d3e4018d1756c3b20d990229e456eae1
Ajaita <ajaitasaini@ajaitas-mbp.lan>
2019-11-06 03:08:25 -0600

    color palette

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ class App extends Component {
   render() {
     const styles = {
       black: (opacity = 1) => `rgba=(16, 23, 26, ${opacity})`,
-      lightGreen: (opacity = 1) => `rgba(66, 115, 71, ${opacity})`,
+      lightGreen: (opacity = 1) => `rgba(88, 123, 127, ${opacity})`,
       darkGreen: (opacity = 1) => `rgba(0, 51, 47, ${opacity})`,
       lightYellow: (opacity = 1) => `rgba(251, 243, 211, ${opacity})`,
       darkYellow: (opacity = 1) => `rgba(230, 178, 43, ${opacity})`,
-      dustyRose: (opacity = 1) => `rgba(146, 20, 44, ${opacity})`,
+      dustyRose: (opacity = 1) => `rgba(142, 164, 210, ${opacity})`,
 
     };
 
